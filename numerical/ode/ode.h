af14d78b9a1572c25a4c64f879c5c292e4937b20
Dennis Lönard <dennis.loenard97@gmx.de>
2025-05-05 14:24:18 +0200

    runge kutta templating

--- numerical/ode/ode.h
+++ numerical/ode/ode.h
@@ -1,3 +1,8 @@
+/*! \file ode.h
+    
+    ordinary differential equation solvers
+*/
+
 #pragma once
 
 #include <cstddef>
@@ -7,17 +12,32 @@
 
 namespace numerical {
     namespace ode {
-        template <typename T, typename U, typename V, typename Func>
-        class RungeKutta {
+        template<typename U, typename T>
+        class ODEResult {
         public:
-            bool dense_output = false;
             std::vector<U> dense_time;
             std::vector<T> dense_y;
 
+            void display_dense_output() {
+                std::size_t i, j;
+                std::size_t N = dense_y[0].size();
+
+                printf("# time, # y\n");
+                for (i = 0; i < dense_time.size(); ++i) {
+                    printf("%f, ", dense_time[i]);
+                    dense_y[i].display_csv();
+                    printf("\n");
+                }
+            }
+        };
+
+        class RungeKutta {
+        public:
+            bool dense_output = false;
+
             RungeKutta() {};
     
-            T run(Func derivative, T y, U t_start, U t_end, U t_step, V params);
-            void display_dense_output();
+            template <typename T, typename U, typename Func, typename... Args> ODEResult<U, T> run(Func derivative, T y, U t_start, U t_end, U t_step, Args&&... params);
         };
     }
 }
--- numerical/ode/runge_kutta.cpp
+++ numerical/ode/runge_kutta.cpp
@@ -1,24 +1,17 @@
-/*! \file ode.cpp
-    
-    ordinary differential equation solvers
-*/
-
 #include "../nvector.h"
 #include "../mnmatrix.h"
 
 namespace numerical {
     namespace ode {
-        template <typename T, typename U, typename V, typename Func>
-        T RungeKutta<T, U, V, Func>::run(Func derivative, T y, U t_start, U t_end, U t_step, V params) {
+        template <typename T, typename U, typename Func, typename... Args>
+        ODEResult<U, T> RungeKutta::run(Func derivative, T y, U t_start, U t_end, U t_step, Args&&... args) {
             U time = t_start;
             T k1, k2, k3, k4;
-
-            dense_time.clear();
-            dense_y.clear();
+            ODEResult<U, T> result;
 
             if (dense_output) {
-                dense_time.push_back(time);
-                dense_y.push_back(y);
+                result.dense_time.push_back(time);
+                result.dense_y.push_back(y);
             }
     
             while (time < t_end) {
@@ -27,34 +20,30 @@ namespace numerical {
                     t_step = t_end - time;
                 }
 
-                k1 = derivative(time, y, params);
-                k2 = derivative(time + t_step / 2.0, y + k1 * (t_step / 2.0f), params);
-                k3 = derivative(time + t_step / 2.0, y + k2 * (t_step / 2.0f), params);
-                k4 = derivative(time + t_step, y + k3 * t_step, params);
+                if constexpr (std::is_invocable_v<Func, U, T>) {
+                    k1 = derivative(time, y);
+                    k2 = derivative(time + t_step / 2.0, y + k1 * (t_step / 2.0f));
+                    k3 = derivative(time + t_step / 2.0, y + k2 * (t_step / 2.0f));
+                    k4 = derivative(time + t_step, y + k3 * t_step);
+                } else if constexpr (std::is_invocable_v<Func, U, T, Args...>) {
+                    k1 = derivative(time, y, std::forward<Args>(args)...);
+                    k2 = derivative(time + t_step / 2.0, y + k1 * (t_step / 2.0f), std::forward<Args>(args)...);
+                    k3 = derivative(time + t_step / 2.0, y + k2 * (t_step / 2.0f), std::forward<Args>(args)...);
+                    k4 = derivative(time + t_step, y + k3 * t_step, std::forward<Args>(args)...);
+                } else {
+                    static_assert(sizeof...(Args) == 0, "Invalid function signature for derivative function");
+                }
                 y = y + (k1 + k2 * 2.0 + k3 * 2.0 + k4) * (t_step / 6.0f);
 
                 if (dense_output) {
-                    dense_time.push_back(time);
-                    dense_y.push_back(y);
+                    result.dense_time.push_back(time);
+                    result.dense_y.push_back(y);
                 }
 
                 time += t_step;
             }
 
-            return y;
-        }
-
-        template <typename T, typename U, typename V, typename Func>
-        void RungeKutta<T, U, V, Func>::display_dense_output() {
-            std::size_t i, j;
-            std::size_t N = dense_y[0].size();
-
-            printf("# time, # y");
-            for (i = 0; i < dense_time.size(); ++i) {
-                printf("%f, ", dense_time[i]);
-                dense_y[i].display_csv();
-                printf("\n");
-            }
+            return result;
         }
     }
 }
